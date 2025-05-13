40aa432f9b72f7f50445ea756093bac16a212046
whiteys1 <147918555+whiteys1@users.noreply.github.com>
2025-05-13 17:42:53 +0900

    초기설정

---
+++ src/test/java/kau_network/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kau_network.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
