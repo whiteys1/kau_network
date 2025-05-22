866d1fc38b848e66599a8bed188b3cae28e99649
suleunky <101116633+suleunky@users.noreply.github.com>
2025-05-22 16:19:46 +0900

    solving 403 error

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -1,22 +1,27 @@
-// js/mypage.js
 window.onload = async function () {
   const token = localStorage.getItem("jwt");
   if (!token) {
     window.location.href = "login.html";
     return;
   }
-  // 서버주소 추후에 추가 예정
-  const response = await fetch(""http://52.79.104.211:8080/api/user/me", {
-    headers: { Authorization: "Bearer " + token },
+
+  const response = await fetch("http://52.79.104.211:8080/auth/me", {
+    headers: {
+      accept: "*/*",
+      Authorization: "Bearer " + token,
+    },
   });
 
   if (response.ok) {
     const user = await response.json();
-    document.getElementById("userInfo").textContent = JSON.stringify(
-      user,
-      null,
-      2
-    );
+    document.getElementById("userInfo").innerHTML = `
+      <ul>
+        <li><b>이름:</b> ${user.name}</li>
+        <li><b>이메일:</b> ${user.email}</li>
+        <li><b>전화번호:</b> ${user.phoneNumber}</li>
+        <li><b>주소:</b> ${user.address}</li>
+      </ul>
+    `;
   } else {
     localStorage.removeItem("jwt");
     window.location.href = "login.html";
